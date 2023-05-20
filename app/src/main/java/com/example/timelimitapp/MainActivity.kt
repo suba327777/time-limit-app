8d36a935cc84d89feeaaf2571cf991dc3dd52372
suba <84484832+suba327777@users.noreply.github.com>
2023-05-20 17:46:57 +0900

    権限が付与されているかの処理を追加

--- app/src/main/java/com/example/timelimitapp/MainActivity.kt
+++ app/src/main/java/com/example/timelimitapp/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.example.timelimitapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+//import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.timelimitapp.ui.components.TextSample
 import com.example.timelimitapp.ui.theme.TimeLimitAppTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,11 +26,23 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     TextSample(name = "Android")
+                    //仮のbutton
+                    /*Button(onClick ={KioskManager.setKioskMode(this)} ){
+                        Text(text = "kioskMode")
+                    }*/
                 }
             }
        }
     }
+
+    override fun onResume() {
+        super.onResume()
+        KioskManager.isAdmin(this)
+    }
+
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
