bc83ba40d591b937ec874f15e4d8f589276f6431
suba <84484832+suba327777@users.noreply.github.com>
2023-05-20 16:22:47 +0900

    権限が付与されていない時の挙動を実装

--- app/src/main/java/com/example/timelimitapp/KioskManager.kt
+++ app/src/main/java/com/example/timelimitapp/KioskManager.kt
@@ -1,6 +1,7 @@
 package com.example.timelimitapp
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.app.admin.DeviceAdminReceiver
 import android.app.admin.DevicePolicyManager
 import android.content.ComponentName
@@ -8,30 +9,51 @@ import android.content.Context
 import android.content.Intent
 
 class KioskManager() {
+    companion object {
+        fun setKioskMode(context: Activity) {
 
-    fun setKioskMode(context:Activity) {
+            val devicePolicyManager =
+                context.getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+            val myDeviceAdmin = ComponentName(context, KioskDeviceAdminReceiver::class.java)
 
-         val devicePolicyManager = context.getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
-         val myDeviceAdmin = ComponentName(context,KioskDeviceAdminReceiver::class.java)
-
-        if(devicePolicyManager.isAdminActive(myDeviceAdmin)){
-            context.startLockTask()
-        }else{
-            context.startActivity(
-                Intent().setComponent(
-                    ComponentName("com.android.settings","com.android.settings.DeviceAdminSettings")
-                )
-            )
-        }
-    }
-
-    class KioskDeviceAdminReceiver:DeviceAdminReceiver(){
-        override fun onEnabled(context: Context, intent: Intent) {
-            super.onEnabled(context, intent)
+            if (devicePolicyManager.isAdminActive(myDeviceAdmin)) {
+                context.startLockTask()
+            }
         }
 
-        override fun onDisabled(context: Context, intent: Intent) {
-            super.onDisabled(context, intent)
+        fun isAdmin(context: Activity) {
+            val devicePolicyManager =
+                context.getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+            val myDeviceAdmin = ComponentName(context, KioskDeviceAdminReceiver::class.java)
+            if (!devicePolicyManager.isAdminActive(myDeviceAdmin)) {
+                val dialog = AlertDialog.Builder(context)
+                    .setTitle("権限を追加してください")
+                    .setMessage("この機能を使用するためには特定の権限が必要です。設定画面を開き、アプリをデバイス管理者に設定してください。")
+                    .setPositiveButton("設定画面に遷移") { dialog, _ ->
+                        context.startActivity(
+                            Intent().setComponent(
+                                ComponentName(
+                                    "com.android.settings",
+                                    "com.android.settings.DeviceAdminSettings"
+                                )
+                            )
+                        )
+
+                        dialog.dismiss()
+                    }
+                    .create()
+                dialog.show()
+            }
+        }
+
+        class KioskDeviceAdminReceiver : DeviceAdminReceiver() {
+            override fun onEnabled(context: Context, intent: Intent) {
+                super.onEnabled(context, intent)
+            }
+
+            override fun onDisabled(context: Context, intent: Intent) {
+                super.onDisabled(context, intent)
+            }
         }
     }
 }
