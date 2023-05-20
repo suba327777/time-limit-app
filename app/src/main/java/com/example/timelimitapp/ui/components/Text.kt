2b5dd43776bd1991cbf1249dd2c0391cdedc985c
sakazumero <moriharu3730221@gmail.com>
2023-05-20 12:17:14 +0900

    ボタン機能を作りました

--- app/src/main/java/com/example/timelimitapp/ui/components/Text.kt
+++ app/src/main/java/com/example/timelimitapp/ui/components/Text.kt
@@ -3,6 +3,7 @@ package com.example.timelimitapp.ui.components
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.material3.Button
 
 @Composable
 fun TextSample(name: String, modifier: Modifier = Modifier) {
