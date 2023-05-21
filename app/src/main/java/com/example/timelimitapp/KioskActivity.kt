0224d649590b6f17601dd09fd0aaf45970ef8690
suba <84484832+suba327777@users.noreply.github.com>
2023-05-21 09:56:31 +0900

    受け取った開始時間と終了時間を表示

--- app/src/main/java/com/example/timelimitapp/KioskActivity.kt
+++ app/src/main/java/com/example/timelimitapp/KioskActivity.kt
@@ -3,13 +3,18 @@ package com.example.timelimitapp
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 
 class KioskActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val startmTime = intent.getStringExtra("startmTime")
+        val endmTime = intent.getStringExtra("endmTime")
         setContent {
-            Text(text = "text")
+            Column() {
+                Text(text = "${startmTime}~${endmTime}")
+            }
         }
     }
 }
